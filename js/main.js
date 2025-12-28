e2130c40873331610ce75eb35378f28d4463f8d7
Валерия Якимова <lerayakimova2407@gmail.com>
2025-12-28 19:49:51 +0500

    Отрисуй меня полностью

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 import { renderPictures } from './pictures-render.js';
 import { photos } from './data.js';
 
+console.log('main.js загружен');
+console.log('Количество фото для отрисовки:', photos.length);
+
 document.addEventListener('DOMContentLoaded', () => {
-  renderPictures(photos);
+    console.log('DOM загружен, начинаю отрисовку...');
+    renderPictures(photos);
+    console.log('Отрисовка завершена');
 });
 
 export { photos };
